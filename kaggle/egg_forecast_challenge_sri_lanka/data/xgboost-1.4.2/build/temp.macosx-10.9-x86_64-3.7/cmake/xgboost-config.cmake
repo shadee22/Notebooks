6b8ff31c288c1b1f5a200c0e28bacf05d9d3c427
Shadeer Sadikeen <shadeersadikeen@gmail.com>
2023-10-30 00:32:32 +0530

    added kaggle

---
+++ kaggle/egg_forecast_challenge_sri_lanka/data/xgboost-1.4.2/build/temp.macosx-10.9-x86_64-3.7/cmake/xgboost-config.cmake
@@ -0,0 +1,48 @@
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was xgboost-config.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+include(CMakeFindDependencyMacro)
+
+set(USE_OPENMP 0)
+set(USE_CUDA 0)
+set(USE_NCCL 0)
+
+find_dependency(Threads)
+if(USE_OPENMP)
+  find_dependency(OpenMP)
+endif()
+if(USE_CUDA)
+  find_dependency(CUDA)
+endif()
+if(USE_NCCL)
+  find_dependency(Nccl)
+endif()
+
+if(NOT TARGET xgboost::xgboost)
+  include(${CMAKE_CURRENT_LIST_DIR}/XGBoostTargets.cmake)
+endif()
+
+message(STATUS "Found XGBoost (found version \"${xgboost_VERSION}\")")
